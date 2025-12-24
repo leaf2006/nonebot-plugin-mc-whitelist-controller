e3de6831472a3a3c2cd462b77ce5a845f3e0ab70
leaf2006 <yeshuo20061031@outlook.com>
2025-12-24 13:24:17 +0800

    更新README.md，添加pip安装说明；修改指令列表命令及帮助信息

--- nonebot_plugin_mc_whitelist_controller/__init__.py
+++ nonebot_plugin_mc_whitelist_controller/__init__.py
@@ -16,18 +16,18 @@ from nonebot import logger
 
 # config = get_plugin_config(Config)
 
-information_helper = on_command("help",aliases={"帮助"},priority=5,block=True)
+information_helper = on_command("指令列表",priority=5,block=True)
 
 @information_helper.handle()
 async def handle_information_helper():
     info_Helper_message = Message([
         "✨这是一个控制管理Minecraft服务器白名单的机器人插件，将mc服务器中的玩家id与QQ号绑定，实现对服务器内所有玩家的追根溯源，支持正版服务器和离线服务器。本插件可以在QQ中将玩家id注册入服务器白名单，同时会生成一个包含每个玩家id与其绑定的QQ号信息的json文件，供服务器管理员参看。✨\n \n",
-        "✍使用方法：✍ \n",
+        "✍指令列表：✍ \n",
         "① /注册 或 /register + [玩家id]：向服务器白名单注册玩家信息（会自动获取发消息者的QQ号进行绑定） \n",
-        "② /帮助 或 /help：查看帮助信息 \n \n",
+        "② /指令列表：查看帮助信息 \n \n",
         "Powered by Nonebot2\n",
-        "Copyright © Leaf developer 2023-2026"  
-    ])
+        "Copyright © Leaf developer 2023-2026"
+    ]) #type:ignore
 
     await information_helper.finish(info_Helper_message)
 
